d5e17e689d5be551202812d0f6d0f81ab382af70
Robert-at-486987442298 <robert@k4media.com>
2022-12-24 22:44:31 +0700

    initial commit

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Angkor_Research_&_Consulting
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'arc' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'arc' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'arc' ), 'arc', '<a href="https://k4media.com/">robert@k4media.com</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,209 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'automattic/underscores',
+  ),
+  'versions' => 
+  array (
+    'automattic/underscores' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'dealerdirect/phpcodesniffer-composer-installer' => 
+    array (
+      'pretty_version' => 'v0.7.2',
+      'version' => '0.7.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c968e542d8843d7cd71de3c5c9c3ff3ad71a1db',
+    ),
+    'eftec/bladeone' => 
+    array (
+      'pretty_version' => '3.52',
+      'version' => '3.52.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a19bf66917de0b29836983db87a455a4f6e32148',
+    ),
+    'gettext/gettext' => 
+    array (
+      'pretty_version' => 'v4.8.8',
+      'version' => '4.8.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '302a00aa9d6762c92c884d879c15d3ed05d6a37d',
+    ),
+    'gettext/languages' => 
+    array (
+      'pretty_version' => '2.10.0',
+      'version' => '2.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4d61d67fe83a2ad85959fe6133d6d9ba7dddd1ab',
+    ),
+    'grogy/php-parallel-lint' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'jakub-onderka/php-parallel-lint' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'mck89/peast' => 
+    array (
+      'pretty_version' => 'v1.15.0',
+      'version' => '1.15.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '733cd8f62dcb8239094688063a92766bbfcbf523',
+    ),
+    'mustache/mustache' => 
+    array (
+      'pretty_version' => 'v2.14.2',
+      'version' => '2.14.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e62b7c3849d22ec55f3ec425507bf7968193a6cb',
+    ),
+    'php-parallel-lint/php-parallel-lint' => 
+    array (
+      'pretty_version' => 'v1.3.2',
+      'version' => '1.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6483c9832e71973ed29cf71bd6b3f4fde438a9de',
+    ),
+    'phpcompatibility/php-compatibility' => 
+    array (
+      'pretty_version' => '9.3.5',
+      'version' => '9.3.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fb324479acf6f39452e0655d2429cc0d3914243',
+    ),
+    'phpcompatibility/phpcompatibility-paragonie' => 
+    array (
+      'pretty_version' => '1.3.2',
+      'version' => '1.3.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bba5a9dfec7fcfbd679cfaf611d86b4d3759da26',
+    ),
+    'phpcompatibility/phpcompatibility-wp' => 
+    array (
+      'pretty_version' => '2.1.4',
+      'version' => '2.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b6c1e3ee1c35de6c41a511d5eb9bd03e447480a5',
+    ),
+    'rmccue/requests' => 
+    array (
+      'pretty_version' => 'v1.8.1',
+      'version' => '1.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '82e6936366eac3af4d836c18b9d8c31028fe4cd5',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.7.1',
+      'version' => '3.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1359e176e9307e906dc3d890bcc9603ff6d90619',
+    ),
+    'symfony/finder' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'eb2355f69519e4ef33f1835bca4c935f5d42e570',
+    ),
+    'wp-cli/i18n-command' => 
+    array (
+      'pretty_version' => 'v2.4.1',
+      'version' => '2.4.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '22f7e6aa6ba23d0b50c45c75386c8151b991477e',
+    ),
+    'wp-cli/mustangostang-spyc' => 
+    array (
+      'pretty_version' => '0.6.3',
+      'version' => '0.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6aa0b4da69ce9e9a2c8402dab8d43cf32c581cc7',
+    ),
+    'wp-cli/php-cli-tools' => 
+    array (
+      'pretty_version' => 'v0.11.16',
+      'version' => '0.11.16.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c32e51a5c9993ad40591bc426b21f5422a5ed293',
+    ),
+    'wp-cli/wp-cli' => 
+    array (
+      'pretty_version' => 'v2.7.1',
+      'version' => '2.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1ddc754f1c15e56fb2cdd1a4e82bd0ec6ca32a76',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7da1894633f168fe244afc6de00d141f27517b62',
+    ),
+    'wptrt/wpthemereview' => 
+    array (
+      'pretty_version' => '0.2.1',
+      'version' => '0.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '462e59020dad9399ed2fe8e61f2a21b5e206e420',
+    ),
+  ),
+);
